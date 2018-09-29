9c474349669db2a3c0708b943b84da7b0fe333a4
Chayan Datta <sonicxxx7@gmail.com>
2018-09-30 03:14:14 +0530

    Recipe List Components created

---
+++ src/app/shopping-list/shooping-edit/shooping-edit.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShoopingEditComponent } from './shooping-edit.component';
+
+describe('ShoopingEditComponent', () => {
+  let component: ShoopingEditComponent;
+  let fixture: ComponentFixture<ShoopingEditComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ShoopingEditComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ShoopingEditComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
